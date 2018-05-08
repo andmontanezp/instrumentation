ac0f4d71e1e162fab58e3a9446d9d7ee4dd39e22
Anderson Montanez <anderson.montanez@globant.com>
2018-05-07 22:33:06 -0300

    Initial Commit

---
+++ app/src/androidTest/java/com/example/anderson/instrumentation/ExampleInstrumentedTest.kt
@@ -0,0 +1,39 @@
+package com.example.anderson.instrumentation
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.espresso.Espresso
+import android.support.test.espresso.action.ViewActions
+import android.support.test.espresso.matcher.ViewMatchers
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+import org.junit.Rule
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+
+    @Rule
+    @JvmField
+    val activity = ActivityTestRule<MainActivity>(MainActivity::class.java)
+
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.example.anderson.instrumentation", appContext.packageName)
+    }
+
+    @Test
+    fun testOnClick() {
+        Espresso.onView(ViewMatchers.withId(R.id.hello)).perform(ViewActions.click())
+    }
+}
